5e71668102d4d1d0a22d611fdebb9daa89084c0d
handonghoo <handonghoo0812@gmail.com>
2023-11-24 16:36:47 +0900

    12주차 진척보고서

--- Sources/main.c
+++ Sources/main.c
@@ -1,64 +1,118 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// 콜백 함수
-size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata) {
-    // 수신한 데이터를 출력하지 않고 무시합니다.
-    return size * nmemb;
-}
+// 환율 정보를 저장하기 위한 구조체
+typedef struct {
+    char base[4]; // 환율 기준 통화
+    char target[4]; // 환율 표시 통화
+    double rate; // 환율
+} ExchangeRate;
 
-double get_exchange_rate(const char* base_currency, const char* target_currency) {
-    FILE* fp;
-    char url[256];
-    char command[512];
-    char response[1024];
-    double exchange_rate = 0.0;
+// 파일에서 환율 정보를 가져오는 함수
+ExchangeRate get_exchange_rate() {
+    // 환율 정보 파일을 열기 위한 파일 포인터
+    FILE* file;
 
-    // API 요청 URL 생성
-    sprintf(url, "https://openexchangerates.org/api/latest.json?app_id=YOUR_API_KEY&base=%s&symbols=%s", base_currency, target_currency);
-
-    // cURL 명령어 실행
-    sprintf(command, "curl -s \"%s\"", url);
-    fp = _popen(command, "r");
-    if (fp != NULL) {
-        // 응답 데이터 읽기
-        fread(response, sizeof(char), sizeof(response) - 1, fp);
-        _pclose(fp);
-
-        // JSON 파싱
-        char* rate_start = strstr(response, "\"") + 1;
-        char* rate_end = strstr(rate_start, "\"");
-        *rate_end = '\0';
-        exchange_rate = atof(rate_start);
+    // 파일을 열지 못하면 오류 출력
+    if (fopen_s(&file, "exchange_rate.txt", "r") != 0) {
+        fprintf(stderr, "환율 정보 파일을 열 수 없습니다.\n");
+        return (ExchangeRate) { 0 };
     }
 
-    return exchange_rate;
+    // 파일에서 한 줄씩 읽기
+    char line[256];
+    char* rate_start = NULL;
+    double exchange_rate = 0.0;
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        // KRW 환율을 찾기 위한 조건
+        rate_start = strstr(line, "\"KRW\":");
+
+        // KRW 환율이 있으면 환율을 저장
+        if (rate_start != NULL) {
+            sscanf_s(rate_start + 7, "%lf", &exchange_rate);
+            break;
+        }
+    }
+
+    // 파일을 닫기
+    fclose(file);
+
+    // 환율이 없으면 오류 출력
+    if (exchange_rate == 0.0) {
+        fprintf(stderr, "환율 정보를 찾을 수 없습니다.\n");
+        return (ExchangeRate) { 0 };
+    }
+
+    // 환율 정보를 구조체에 저장하여 반환
+    return (ExchangeRate) { "USD", "KRW", exchange_rate };
 }
 
-int main() {
-    const char* base_currency = "USD";  // 기준 통화
-    const char* target_currency = "KRW";  // 변환할 통화
+// 주별 세율을 구하는 함수
+double get_state_tax_rate(char state) {
+    // 주별 세율
+    double state_tax = 0.0;
 
-    double exchange_rate = get_exchange_rate(base_currency, target_currency);
-    printf("1 %s = %.2f %s\n", base_currency, exchange_rate, target_currency);
+    // 주별 세율을 구하기 위한 switch 문
+    switch (state) {
+    case 'A':
+        state_tax = 4.0;
+        break;
+    case 'B':
+        state_tax = 5.0;
+        break;
+    case 'C':
+        state_tax = 5.6;
+        break;
+    case 'D':
+        state_tax = 6.5;
+        break;
+    case 'E':
+        state_tax = 7.25;
+        break;
+    case 'F':
+        state_tax = 2.9;
+        break;
+    case 'G':
+        state_tax = 6.35;
+        break;
+    case 'H':
+        state_tax = 0;
+        break;
+    case 'I':
+        state_tax = 6.0;
+        break;
+    default:
+        break;
+    }
 
-
-    //  API 키값 미확보로 인한 임의의 환율 구현
-     // 환율을 변수에 저장합니다.
-    float exchange_rate = 1225.00;
-
-    // 한화로 물건의 가격을 입력받습니다.
-    float won = 0.0;
-    printf("물건의 가격을 한화로 입력하세요: ");
-    scanf("%f", &won);
-
-    // 한화를 달러로 변환합니다.
-    float dollar = won / exchange_rate;
-
-    // 달러로 변환된 가격을 출력합니다.
-    printf("물건의 가격은 %.2f 달러입니다.\n", dollar);
-
-    return 0;
+    // 주별 세율을 반환
+    return state_tax;
 }
+
+// 총 비용을 계산하는 함수
+double calculate_total_cost(double amount, double exchange_rate, int duty_free, int tip, char state) {
+    // 총 비용
+    double total_cost = amount;
+
+    // 환율을 적용하여 총 비용을 계산
+    total_cost *= exchange_rate;
+
+    // 면세 여부에 따라 총 비용을 계산
+    if (duty_free) {
+        total_cost *= 1.15;
+    }
+
+    // 팁 비율을 적용하여 총 비용을 계산
+    double tip_rate = tip / 100.0;
+    total_cost += total_cost * tip_rate;
+
+    // 주별 세율을 적용하여 총 비용을 계산
+    double state_tax = get_state_tax_rate(state);
+    total_cost += total_cost * state_tax;
+
+    // 총 비용을 반환
+    return total_cost;
+}
+
